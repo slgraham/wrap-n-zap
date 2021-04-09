b7f2cac999a2ed8321ca18d1d0d93c0c0c3a5ff5
spengrah <code@spengrah.xyz>
2021-04-08 20:32:56 -0500

    factor out interface

--- contracts/WrapNZap.sol
+++ contracts/WrapNZap.sol
@@ -2,11 +2,7 @@
 
 pragma solidity ^0.8.1;
 
-interface IWrappedETH {
-    function deposit() external payable;
-
-    function transfer(address dst, uint256 wad) external returns (bool);
-}
+import "./interfaces/IWrappedETH.sol";
 
 contract WrapNZap {
     address public zappee;
