edba100f3927692c4700b3b45c672dca394e2fbc
spengrah <code@spengrah.xyz>
2021-04-13 20:26:52 -0500

    janky `poke` tests

--- test/WrapNZap.js
+++ test/WrapNZap.js
@@ -16,6 +16,35 @@ describe('WrapNZap', function () {
 	let addr2;
 	let addrs;
 
+	it('poke triggers zap if balance > 0', async function () {
+		[owner, zappee, addr1, addr2, ...addrs] = await ethers.getSigners();
+		await addr2.sendTransaction({
+			to: '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9', // obtained by deploying wrapNZap and getting its address out of band
+			value: 500,
+		});
+		await expect(
+			await ethers.provider.getBalance(
+				'0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9'
+			)
+		).to.equal(500);
+		mockWrappedToken = await deployMockContract(owner, IWrappedETH.abi);
+
+		WrapNZapFactory = await ethers.getContractFactory('WrapNZap');
+		wrapNZap = await WrapNZapFactory.deploy(
+			zappee.address,
+			mockWrappedToken.address
+		);
+		await wrapNZap.deployed();
+		await mockWrappedToken.mock.deposit.returns();
+		await mockWrappedToken.mock.transfer.returns(true);
+
+		await wrapNZap.poke();
+
+		await expect(
+			await ethers.provider.getBalance(wrapNZap.address)
+		).to.equal(0);
+	});
+
 	beforeEach(async function () {
 		[owner, zappee, addr1, addr2, ...addrs] = await ethers.getSigners();
 
@@ -30,6 +59,7 @@ describe('WrapNZap', function () {
 	});
 
 	it('deploys with correct data', async function () {
+		console.log(wrapNZap.address);
 		expect(await wrapNZap.zappee()).to.equal(zappee.address);
 		expect(await wrapNZap.wrapper()).to.equal(mockWrappedToken.address);
 	});
@@ -58,5 +88,11 @@ describe('WrapNZap', function () {
 			[mockWrappedToken, addr1],
 			[200, -200]
 		);
+	});
+
+	it('reverts poke if no balance', async function () {
+		const receipt = wrapNZap.poke();
+
+		await expect(receipt).to.be.revertedWith('WrapNZap: no balance');
 	});
 });
